85bd0404bfad9c217f0d3f5161d2382ebc37135e
utkarshyadav009 <ybhavna00@gmail.com>
2024-09-03 12:06:49 +0100

    Added Portfolio files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      base: ["Quicksand", "sans-serif"],
+      times: ["Times New Roman", "serif"],
+      title: ["Belgrano", "serif"]
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
